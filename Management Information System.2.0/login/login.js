37621d005e29f20d42a412c342dd8d2645f5d116
Marksukii <153151754+Marksukii@users.noreply.github.com>
2023-12-07 11:30:33 +0800

    Add files via upload

---
+++ Management Information System.2.0/login/login.js
@@ -0,0 +1,20 @@
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
+
+const admins = {
+  admin: "admin",
+};
+
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const usernameValue = username.value;
+  const passwordValue = password.value;
+
+  if (admins[usernameValue] === passwordValue) {
+    
+    window.location.href = "Dashboard.html";
+  } else {
+    alert("Wrong username or password");
+  }
+});
